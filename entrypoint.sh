80312b431f46ac6594c2a7708cc2202ff96513bc
Jeremie Vallee <jeremie.vallee@gmail.com>
2020-05-17 16:52:24 +0200

    Testing upload via SFTP

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,5 @@ set -eu
 
 SFTP_COMMANDS_FILE="/tmp/sftp"
 
-echo "test"
-echo $JEJE_TEST
-echo "Hello, ${1}!"
-time=$(date)
-ls -lh /github/workspace
-
-printf "%s" "ls -lh" > $SFTP_COMMANDS_FILE
+printf "%s" "put -r $3/*" > $SFTP_COMMANDS_FILE
 sshpass -e sftp -o BatchMode=no -o StrictHostKeyChecking=no -b - $1@$2 < $SFTP_COMMANDS_FILE
-echo "::set-output name=anothertest::$time"
