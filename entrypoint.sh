2ad45450544cfd2a48d1a5e85d66c9513bba44c6
Jeremie Vallee <jeremie.vallee@gmail.com>
2020-05-17 16:07:31 +0200

    Testing SFTP

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,8 @@ echo "Hello, ${1}!"
 time=$(date)
 ls -lh /github/workspace
 
+SFTP_COMMANDS_FILE = '../sftp'
+printf "%s" "ls -lh" > $SFTP_COMMANDS_FILE
+sshpass -e sftp -b $SFTP_COMMANDS_FILE -o StrictHostKeyChecking=no $1@$2
+
 echo "::set-output name=anothertest::$time"
