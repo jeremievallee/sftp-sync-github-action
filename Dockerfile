20bd5fd35c9d6b6a06770a442655602b1ccc54c7
Jeremie Vallee <jeremie.vallee@gmail.com>
2020-05-17 13:45:28 +0200

    testing evars

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM alpine:3.11
 
 RUN apk update
-RUN apk add --no-cache openssh bash
+RUN apk add --no-cache openssh bash sshpass
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -l
+set -eu
 
+echo "test"
+echo $JEJE_TEST
 echo "Hello, ${1}!"
 time=$(date)
-
 ls -lh /github/workspace
 
 echo "::set-output name=anothertest::$time"
